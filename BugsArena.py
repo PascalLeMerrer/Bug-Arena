707894ed2034680b7a7bcd10b2d46497f89b2cef
Alternate <pascal.lemerrer@gmail.com>
2012-03-18 19:35:54 +0100

    new sprites

--- BugsArena.py
+++ BugsArena.py
@@ -27,11 +27,11 @@ class HomeLayer(Layer):
 
     def __init__(self):
         super(HomeLayer, self).__init__()
-        window_size = director.get_window_size()
+        screen_width, screen_height = director.get_window_size()
         self.text_title = pyglet.text.Label("Play",
             font_size=32,
-            x=window_size[0] / 2,
-            y=window_size[1] / 2,
+            x = screen_width / 2,
+            y = screen_height / 2,
             anchor_x='center',
             anchor_y='center')
 
@@ -57,8 +57,7 @@ class BugLayer(Layer):
 
         cell_width = 100    # ~ bug image width * 1,25
         cell_height = 190   # ~bug image height * 1.25
-        screen_width = director.get_window_size()[0]
-        screen_height = director.get_window_size()[1]
+        screen_width, screen_height = director.get_window_size()
         self.collision_manager = collision_model.CollisionManagerGrid(
                                                         0.0, screen_width,
                                                         0.0, screen_height,
@@ -74,11 +73,7 @@ class BugLayer(Layer):
 
         for bug in self.collision_manager.objs_touching_point(mouse_x,
                                                               mouse_y):
-            self.collision_manager.remove_tricky(bug)
-            active_bug_list.remove(bug)
-            bug.stop()
-            bugLayer.remove(bug)
-            bug_pool.append(bug)
+            kill_bug(bug)
 
 
 class Bug(Sprite):
@@ -86,8 +81,13 @@ class Bug(Sprite):
 
     def __init__(self):
         self.duration = random.randint(2, 8)
-        bugSpriteSheet = pyglet.resource.image('SpriteSheet.png')
-        bugGrid = pyglet.image.ImageGrid(bugSpriteSheet, 2, 3)
+        if(self.duration < 5):
+            image = 'bug1-small.png'
+        else:
+            image = 'bug2-small.png'
+
+        bugSpriteSheet = pyglet.resource.image(image)
+        bugGrid = pyglet.image.ImageGrid(bugSpriteSheet, 1, 6)
         animation_period = max(self.duration / 100, 0.05)  # seconds
 
         animation = bugGrid.get_animation(animation_period)
@@ -95,7 +95,6 @@ class Bug(Sprite):
 
         screen_height = director.get_window_size()[1]
 
-        self.scale = 0.5
         rect = self.get_rect()
 
         self.speed = (screen_height + rect.height) / self.duration
@@ -109,9 +108,14 @@ class Bug(Sprite):
             bug to go to the bottom of the screen '''
         self.spawn()
 
-        wasColliding = self.respawn_on_collision()
-        while wasColliding:
-            wasColliding = self.respawn_on_collision()
+        self.is_colliding = self.respawn_on_collision()
+        collision_counter = 1 if self.is_colliding else 0
+        while self.is_colliding:
+            self.is_colliding = self.respawn_on_collision()
+            if self.is_colliding:
+                collision_counter += 1
+            if collision_counter > 3:
+                break
 
         self.rotation = -self.duration
         rotate = RotateBy(self.duration * 2, 1)
@@ -136,13 +140,13 @@ class Bug(Sprite):
             redraw it at another random place on top of screen
             returns True when it was colliding
         '''
-        wasColliding = False
+        is_colliding = False
         for other in active_bug_list:
             if bugLayer.collision_manager.they_collide(self, other):
                 self.spawn()
-                wasColliding = True
+                is_colliding = True
                 break
-        return wasColliding
+        return is_colliding
 
     def move_by(self, dx, dy):
         ''' moves the bug
@@ -178,23 +182,17 @@ def update(dt, *args, **kwargs):
                 rotation = bug.rotation - 360
             else:
                 rotation = bug.rotation
+
             dx = - dy * math.sin(rotation)
-            # print "rotation: ", bug.rotation, " dx: " , dx, " dy: ", dy
             if bug.x - bug.width < 0 and dx < 0:
                 dx = 0
             elif bug.x + bug.width > screen_width and dx > 0:
                 dx = 0
 
             bug.move_by(dx, -dy)
+            
             if bug.position[1] < 0:
-                active_bug_list.remove(bug)
-                bug.stop()
-                try:
-                    bugLayer.collision_manager.remove_tricky(bug)
-                except:
-                    pass
-                bugLayer.remove(bug)
-                bug_pool.append(bug)
+                kill_bug(bug)
 
 
 def create_bug(dt, *args, **kwargs):
@@ -208,7 +206,18 @@ def create_bug(dt, *args, **kwargs):
     bugLayer.collision_manager.add(bug)
     bug.start()
     active_bug_list.append(bug)
+    if bug.is_colliding:
+        kill_bug(bug) # the bug did not find any free place to spawn
 
+def kill_bug(bug):
+    active_bug_list.remove(bug)
+    bug.stop()
+    try:
+        bugLayer.collision_manager.remove_tricky(bug)
+    except:
+        pass
+    bugLayer.remove(bug)
+    bug_pool.append(bug)
 
 if __name__ == "__main__":
 
@@ -224,11 +233,10 @@ if __name__ == "__main__":
         bug_pool.append(Bug())
 
     homeLayer = HomeLayer()
-    colorLayer1 = ColorLayer(0, 255, 255, 255)
-    homeScene = Scene(colorLayer1, homeLayer)
+    colorLayer = ColorLayer(128, 16, 16, 255)
+    homeScene = Scene(colorLayer, homeLayer)
 
     bugLayer = BugLayer()
-    colorLayer2 = ColorLayer(128, 16, 16, 255)
     gameScene = Scene(bugLayer)
 
     director.run(homeScene)
